5c523ebe308b7adfba1d3bbc9368b5ee934851d0
LugitaSandikaPrameswari <lugitasandikap@gmail.com>
2024-10-17 23:33:43 +0700

    modul 5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,22 +3,90 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Kelas;
+use App\Models\UserModel;
 
 class UserController extends Controller
 {
-    public function create(){
-        return view('create_user');
+    public $userModel;
+    public $kelasModel;
 
-        
-    }
-    public function store(Request $request) {
+    public function create()
+    {
+        $kelasModel = new Kelas();
+        $kelas = $this->kelasModel->getKelas();
         $data = [
-            'nama' => $request->input('nama'),
-            'kelas' => $request->input('kelas'),
-            'npm' => $request->input('npm'),
+            'title' => 'Create User',
+            'kelas' => $kelas,
+        ];
+        return view('create_user', $data);
+    }
+
+    public function __construct(){
+        $this->userModel = new UserModel();
+        $this->kelasModel = new Kelas();
+    }
+
+    public function index(){
+        $data = [
+            'title' => 'Create User',
+            'kelas' => $this->userModel->getUser(),
         ];
 
-        return view('profile', $data);
+        $users = UserModel::with('kelas')->get();
+
+        return view('list_user', compact('users'), $data);
+    }
+
+    public function store(Request $request)
+    {
+        $this->userModel->create([
+            'nama' => $request->input('nama'),
+            'npm' => $request->input('npm'),
+            'kelas_id' => $request->input('kelas_id'),
+            ]);
+            return redirect()->to('/user');
+    }
+
+
+    public function uploadProfilePicture(Request $request)
+    {
+        
+        $request->validate([
+            'profile_picture' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        
+        $file = $request->file('profile_picture');
+
+     
+        $fileName = time().'_'.$file->getClientOriginalName();
+
+      
+        $file->move(public_path('assets/img'), $fileName);
+
+     
+        $profile_picture_path = 'assets/img/' . $fileName;
+
+        
+        return back()->with([
+            'profile_picture' => $profile_picture_path,
+            'nama' => $request->input('nama'),
+            'npm' => $request->input('npm'),
+            'nama_kelas' => $request->input('kelas_id') 
+        ]);
+    }
+
+    public function showProfile($id)
+    {
+      
+        $user = User::findOrFail($id);
+
+        return view('profile', [
+            'nama' => $user->nama,
+            'npm' => $user->npm,
+            'nama_kelas' => $user->kelas->nama_kelas ?? 'Kelas tidak ditemukan',
+            'profile_picture' => session('profile_picture', 'public/assets/img/Bromo DHR 2.jpg'), 
+        ]);
     }
 }
-
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,32 @@
 <?php
 
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController; // pastikan ada import ini
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {return view('welcome');});
+// Route untuk tugas 2
 Route::get('/profile', [ProfileController::class, 'profile']);
-//Route::get('/profile/{nama}', [ProfileController::class, 'profile']);
-//Route::get('/profile', [ProfileController::class, 'profile']);
+Route::get('/user/profile', [ProfileController::class, 'profile']);
 
-Route::get('/user/profile', [UserController::class, 'profile']);
-Route::get('/user/create', [UserController::class, 'create']);
-
-Route::get('/user/create', [UserController::class, 'create']);
+// Route untuk tugas 3 & 4
+Route::get('/user/create', function () {return view('create_user');});
 Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
+Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
+Route::post('/profile/upload', [ProfileController::class, 'uploadProfilePicture'])->name('upload.profile.picture');
+
+// Route untuk tugas 5
+Route::get('/user', [UserController::class, 'index'])->name('user.index');
+Route::resource('users', UserController::class);
+Route::get('/user/edit/{id}', [UserController::class, 'edit'])->name('users.edit');
+Route::delete('/user/delete/{id}', [UserController::class, 'destroy'])->name('users.destroy');
